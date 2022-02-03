f14665f3adbaa06717fd56251abfaa010f3e67f9
kjdchapman <kjdchapman@gmail.com>
2022-02-03 12:02:18 +0000

    Progress w/ensemble session 1

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,10 @@
 class Fizzbuzz
-  def execute; end
+  def execute(number);
+    if number % 3 == 0
+      return "fizz"
+    elif number %
+    else
+      return number
+    end
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,26 @@
+
 require 'rspec'
 require './fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'works' do
     fizzbuzz = Fizzbuzz.new
-    expect(fizzbuzz.execute).to eq(nil)
+    expect(fizzbuzz.execute(1)).to eq(1)
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns fizz' do
+    fizzbuzz = Fizzbuzz.new
+    expect(fizzbuzz.execute(3)).to eq("fizz")
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns fizz' do
+    fizzbuzz = Fizzbuzz.new
+    expect(fizzbuzz.execute(5)).to eq("buzz")
+  end
+end
+
+
